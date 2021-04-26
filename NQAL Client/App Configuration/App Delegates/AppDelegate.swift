3c9c7d3368cfd5b77e9c344ee3201bc51e104632
islamXdeceloper <islamxdeveloper@gmail.com>
2021-04-26 23:54:52 +0400

    NQAl App

---
+++ NQAL Client/App Configuration/App Delegates/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  NQAL Client
+//
+//  Created by Eslam Ahmed on 26/04/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    var window:UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        print("بسم الله الرحمن الرحيم")
+        return true
+    }
+
+
+}
+
